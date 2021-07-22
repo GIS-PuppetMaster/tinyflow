a9377521a6dcba348f8bc3b0d01474398af21e23
zkx741481546 <zkx741481546@live.com>
2021-07-23 00:12:47 +0800

    实验代码V1.0

--- tests/Experiment/lab1.py
+++ tests/Experiment/lab1.py
@@ -40,38 +40,38 @@ def Experiment1():
     net_names = ['VGG', 'InceptionV3', 'InceptionV4', 'ResNet', 'DenseNet']
     budget = {
         'VGG': {
-            1: {2: 0.22208359326263252, 16: 0.35208730663276117},
-            2: {2: 0.19237962387315288},
-            3: {2: 0.27293590820311870}
+            1: {2: 0.2174494900279348, 16: 0.35648782684120267},
+            2: {2: 0.21485818067033183},
+            3: {2: 0.19819229733851573}
         },
         'InceptionV3': {
-            1: {2: 0.32010243277848915, 16: 0.61289546493444},
-            2: {2: 0.32633013374338454},
-            3: {2: 0.35945254515962777}
+            1: {2: 0.31681040272964184, 16: 0.6304005544632028},
+            2: {2: 0.2752777245166674},
+            3: {2: 0.27398579361625057}
         },
         'InceptionV4': {
-            1: {2: 0.5225409836065574, 16: 0.6867833038464058},
-            2: {2: 0.45604206164519817},
-            3: {2: 0.5785264272890184}
+            1: {2: 0.5019070066423826, 16: 0.6893718558970763},
+            2: {2: 0.46673445779697914},
+            3: {2: 0.4388073557438008}
         },
         'ResNet': {
-            1: {2: 0.7243808225403319, 16: 0.7243808225403319},
-            2: {2: 0.37206134328305823},
-            3: {2: 0.41238891975049113}
+            1: {2: 0.26281338767444734, 16: 0.7066612562202336},
+            2: {2: 0.24516440969028397},
+            3: {2: 0.28701103881578344}
         },
         'DenseNet': {
-            1: {2: 0.3807455664133188, 16: 0.7594717471314137},
-            2: {2: 0.3228169116367175},
-            3: {2: 0.31907608729118836}
+            1: {2: 0.39732987036657325, 16: 0.7603964692004176},
+            2: {2: 0.24516440969028397},
+            3: {2: 0.20844425426073546}
         }
     }
     for net_id in range(5):
-        repeat_times = 1
+        repeat_times = 3
         print("Experiment1 start")
         net_name = net_names[net_id]
         for i, num_net in enumerate([1, 1, 2, 3]):
-            if i != 2:
-                continue
+            # if i != 2:
+            #     continue
             if i == 0:
                 batch_size = 16
                 net_name_ = net_name
@@ -87,13 +87,13 @@ def Experiment1():
                 # net_id = random.randint(0, 4) #net_id随机选取网络种类 0:vgg16, 1:inceptionv3, 2:inceptionv4, 3:resNet, 4:denseNet
                 nets.append(net_id)
             print("选取的网络", list(map(lambda x: net_names[x], nets)))
-            vanilla_max_memory = 8000
+            vanilla_max_memory = 0
             need_tosave_list = []
             for t in range(repeat_times):
                 print(f'repeat_times:{t}')
                 for type in range(3):  # type是调度方式的选择, 0.不调度 1.capuchin 2.vdnn
-                    if type != 2:
-                        continue
+                    # if type != 2:
+                    #     continue
                     need_tosave = 0
                     if type == 1:
                         bud = vanilla_max_memory * (1 - budget[net_name][num_net][batch_size])
@@ -118,7 +118,7 @@ def Experiment1():
                                 size -= need_sqrt * need_sqrt
                         print('finish extra matrix generation')
                     job_pool = [
-                        generate_job(num_step=5, net_id=net_id, type=type, batch_size=batch_size, path=path, file_name=f"_repeat_time={t}_net_order={i}", need_tosave=need_tosave) for
+                        generate_job(num_step=50, net_id=net_id, type=type, batch_size=batch_size, path=path, file_name=f"_repeat_time={t}_net_order={i}", need_tosave=need_tosave) for
                         i, net_id in enumerate(nets)]
                     for job in job_pool:
                         job.start()
